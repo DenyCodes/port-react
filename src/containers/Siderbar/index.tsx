7a112c39175b5581b1f62c94b8a49b1a069ca1bc
Denis Oliveira <denisdev3loper@gmail.com>
2023-12-21 00:09:41 -0300

    estilizando sidebar

--- src/containers/Siderbar/index.tsx
+++ src/containers/Siderbar/index.tsx
@@ -1,9 +1,19 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, Sidebarcontainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/DenyCodes.png" />
-    <Titulo fontSize={20}>Denis Oliveira</Titulo>
+    <Sidebarcontainer>
+      <Avatar />
+      <Titulo fontSize={20}>Denis Oliveira</Titulo>
+      <Paragrafo>Denis Oliveira</Paragrafo>
+      <Descricao tipo="principal" FontSize={12}>
+        Desenvolvedor FullStack
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </Sidebarcontainer>
   </aside>
 )
 export default Sidebar
