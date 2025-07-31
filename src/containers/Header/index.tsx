669167f2ff6e547990d99f9a88e47e6b11033e09
Denis Oliveira <denisdev3loper@gmail.com>
2025-07-31 18:35:22 -0300

    material-icons

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -2,9 +2,12 @@ import { Navbar, Li, Banner, BottonCV, Icon, Ul, ligt } from './styles'
 import Background from '../../images/Denis Oliveira (2).png'
 import light from '../../images/Icon.svg'
 import DownloadButton from '../../components/Download'
+import Lightmode from '@mui/icons-material/LightMode'
+
 type Props = {
   trocaTema: () => void
 }
+
 const Header = (props: Props) => (
   <div>
     <Navbar>
@@ -16,7 +19,21 @@ const Header = (props: Props) => (
           <div>
             <a href="https://github.com/DenyCodes?tab=repositories">Projetos</a>
           </div>
-          <img src={light} alt="icon" onClick={props.trocaTema} />
+
+          {/* Ícone como fonte de material icons */}
+          <Lightmode
+            className="material-icons-outlined"
+            onClick={props.trocaTema}
+            style={{
+              fontSize: 40,
+              color: 'white',
+              cursor: 'pointer',
+              transition: 'color 0.3s'
+            }}
+            onMouseEnter={(e) => (e.currentTarget.style.color = 'yellow')}
+            onMouseLeave={(e) => (e.currentTarget.style.color = 'white')}
+          />
+
           <BottonCV>
             <DownloadButton />
           </BottonCV>
